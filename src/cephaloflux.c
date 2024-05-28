cf71f485f3f989d56305678728684f850d0ff97f
temportalflux <dustin.yost.t@gmail.com>
2024-05-27 20:44:00 -0400

    adjust logging decl

--- src/cephaloflux.c
+++ src/cephaloflux.c
@@ -1,6 +1,6 @@
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_DECLARE(cephaloflux, CONFIG_ZMK_LOG_LEVEL);
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/events/keycode_state_changed.h>
 #include <zmk/events/layer_state_changed.h>
