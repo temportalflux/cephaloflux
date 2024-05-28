f2d7ea68900b7eb140925c16e958c569f07a1483
temportalflux <dustin.yost.t@gmail.com>
2024-05-27 20:26:19 -0400

    Add debug logging

--- src/cephaloflux.c
+++ src/cephaloflux.c
@@ -8,6 +8,15 @@ LOG_MODULE_DECLARE(cephaloflux, CONFIG_ZMK_LOG_LEVEL);
 int handle_keycode_event(const zmk_event_t *eh) {
 	const struct zmk_keycode_state_changed *ev = as_zmk_keycode_state_changed(eh);
 
+	// cause a key press
+	// sampled from https://github.com/zmkfirmware/zmk/blob/main/app/src/behaviors/behavior_key_repeat.c#L80
+	// ```
+	// zmk_keycode_state_changed event;
+	// raise_zmk_keycode_state_changed(event);
+	// ```
+
+	LOG_DBG("cephaloflux handle_keycode_event");
+
 	return ZMK_EV_EVENT_BUBBLE; // to allow the event to continue
 	//return ZMK_BEHAVIOR_OPAQUE; // to consume
 }
@@ -17,7 +26,11 @@ ZMK_SUBSCRIPTION(cephaloflux_keycode_handler, zmk_keycode_state_changed);
 
 int handle_layer_event(const zmk_event_t *eh) {
 	const struct zmk_layer_state_changed *ev = as_zmk_layer_state_changed(eh);
+
+	// `WRITE_BIT(?, ?, bool)` sends data over HID?
 	
+	LOG_DBG("cephaloflux handle_layer_event");
+
 	return ZMK_EV_EVENT_BUBBLE; // to allow the event to continue
 	//return ZMK_BEHAVIOR_OPAQUE; // to consume
 }
